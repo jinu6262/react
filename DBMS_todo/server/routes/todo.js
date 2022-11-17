1551be26d243f37274b4ebb8afb23b92972054e7
jinu6262 <kimjinu2540@naver.com>
2022-11-17 22:36:50 +0900

    [Add] 11/12, 11/13 수업내용 추가

---
+++ DBMS_todo/server/routes/todo.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import { jwtAuth } from '../middlewares/jwtAuth.js';
+import { TodoService } from '../services/todoService.js';
+const router = express.Router();
+
+router.post('/test', jwtAuth, TodoService.create);
+router.post('/', TodoService.create);
+router.get('/', TodoService.read);
+router.put('/:todoId', TodoService.update);
+router.delete('/:todoId', TodoService.delete);
+
+export default router;
